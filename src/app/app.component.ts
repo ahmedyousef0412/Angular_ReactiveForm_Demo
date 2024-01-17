84b01e96f98fa1160f9367e6bf518996d10796be
ahmedyousef0412 <37086138+ahmedyousef0412@users.noreply.github.com>
2024-01-17 10:02:24 +0200

    Add Custom Validations and use from group from a colllection of some control.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
@@ -17,35 +17,69 @@ export class AppComponent implements OnInit {
 
   registrationForm: FormGroup;
 
-
+  disableSubmitBtn :boolean = true;
   ngOnInit(): void {
 
     this.registrationForm = new FormGroup({
 
-      firstname: new FormControl(null),
-      lastname: new FormControl(null),
-      email: new FormControl(null),
+      firstname: new FormControl(null,[
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)
+      ]),
+      lastname: new FormControl(null,[
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(12)
+      ]),
+      email: new FormControl(null,[
+         Validators.email,
+         Validators.required
+        ]),
+
       username: new FormControl(null),
       dob: new FormControl(null),
       gender: new FormControl('male'),
 
-      street: new FormControl(null),
-      country: new FormControl('India'),
-      city: new FormControl(null),
-      region: new FormControl(null),
-      postal: new FormControl(null)
-
-
+      address:new FormGroup({
+        street: new FormControl(null ,Validators.required),
+        country: new FormControl(null,Validators.required),
+        city: new FormControl(null,Validators.required),
+        region: new FormControl(null,Validators.required),
+        postal: new FormControl(null,Validators.required)
+      })    
+      
 
     });
 
 
   }
-
-
-
+  public get firstName(): FormControl {
+    return this.registrationForm.get('firstname') as FormControl;
+  }
+  public get street(): FormControl {
+    return this.registrationForm.get('address.street') as FormControl;
+  }
+  public get country(): FormControl {
+    return this.registrationForm.get('address.country') as FormControl;
+  }
+  public get city(): FormControl {
+    return this.registrationForm.get('address.city') as FormControl;
+  }
+  
+  public get region(): FormControl {
+    return this.registrationForm.get('address.region') as FormControl;
+  }
+  public get postal(): FormControl {
+    return this.registrationForm.get('address.postal') as FormControl;
+  }
   onFormSubmitted(){
-    console.log(this.registrationForm.value.firstname);
+    // console.log(this.registrationForm.value.address.region);
+     console.log(this.registrationForm.value);
     
+  }
+
+ get isFormValid(){
+   return this.registrationForm.invalid;
   }
 }
