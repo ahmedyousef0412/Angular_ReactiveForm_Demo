2cb11012d2800b35b1a4914c7121fd624a077273
ahmedyousef0412 <37086138+ahmedyousef0412@users.noreply.github.com>
2024-01-17 07:43:01 +0200

    Create Reactive Form by using FormGroup and FormControl using formControlName in html to bind on formControl
    in tag from using [formGroup] ="reactiveFormName" and
    (ngSubmit)="functionToSubmit()">
    
    and the submit input should be type of
    submit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Angular_ReactiveForm_Demo';
+
+  registrationForm: FormGroup;
+
+
+  ngOnInit(): void {
+
+    this.registrationForm = new FormGroup({
+
+      firstname: new FormControl(null),
+      lastname: new FormControl(null),
+      email: new FormControl(null),
+      username: new FormControl(null),
+      dob: new FormControl(null),
+      gender: new FormControl('male'),
+
+      street: new FormControl(null),
+      country: new FormControl('India'),
+      city: new FormControl(null),
+      region: new FormControl(null),
+      postal: new FormControl(null)
+
+
+
+    });
+
+
+  }
+
+
+
+  onFormSubmitted(){
+    console.log(this.registrationForm.value.firstname);
+    
+  }
 }
